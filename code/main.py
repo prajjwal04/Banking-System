72ee84ee3324722d401ac4c2e10b0339739ea6b3
prajjwal04 <prajjwal.agarwal992@gmail.com>
2020-04-10 05:05:43 +0530

    splitted the dataset into training and test set

--- code/main.py
+++ code/main.py
@@ -30,3 +30,7 @@ X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
